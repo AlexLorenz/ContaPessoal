c42842e92100f3ac6fb2959dab651a7062a66e2b
Alex Lorenz <alexlorenz95@gmail.com>
2019-11-11 17:13:49 -0300

    adicionando
    Adicionando referencia e categoria a pagamento

--- pagamento.py
+++ pagamento.py
@@ -1,7 +1,8 @@
 class Pagamento:                               #Cria a Classe "Pagamento"
     def __init__(self, data, nome, valor):     #Cria o método construtor passando como parâmetro a data, nome e valor
         self._data = data                      #
-        self._nome = nome                      #
+        self._categoria = nome                      #
+        self._referencia = 'fatura'
         self._valor = valor                    #
 
     @property                                  #
@@ -9,15 +10,19 @@ class Pagamento:                               #Cria a Classe "Pagamento"
         return self._data                      #Retorna a variável "data"
 
     @property                                  #
-    def nome(self):                            #Cria o método "nome"
-        return self._nome                      #Retorna a variável "nome"
+    def categoria(self):                            #Cria o método "nome"
+        return self._categoria                      #Retorna a variável "nome"
+
+    @property
+    def referencia(self):
+        return self._referencia
 
     @property                                  #
     def valor(self):                           #Cria o método "valor"
         return self._valor                     #Retorna a variável "valor"
 
     def __str__(self):
-        return self._data + ' | ' + self._nome + ' | ' + str(self._valor)
+        return self._data + ' | ' + self._categoria + ' | ' + self._referencia + ' | ' + str(self._valor)
 
 ###################### Teste ########################
 '''
